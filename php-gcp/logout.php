310f0c72e6c9499f0c537dfaf4c71bda67ebbd2c
Jack Xu <503600265@qq.com>
2021-11-25 15:16:16 -0500

    login feature

---
+++ php-gcp/logout.php
@@ -0,0 +1,17 @@
+
+<?php
+
+//logout.php
+
+include('config.php');
+
+//Reset OAuth access token
+$google_client->revokeToken();
+
+//Destroy entire session data.
+session_destroy();
+
+//redirect page to index.php
+header('location:index.php');
+
+?>
